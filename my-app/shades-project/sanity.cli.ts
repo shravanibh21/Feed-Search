78605ce6faff704dda8ec74d14be7651bde68751
Shravani Bhujbal <shravanibh21@gmail.com>
2024-07-08 17:59:34 +0530

    Removed projectID

--- my-app/shades-project/sanity.cli.ts
+++ my-app/shades-project/sanity.cli.ts
@@ -1,5 +1,4 @@
 import {defineCliConfig} from 'sanity/cli'
-
 export default defineCliConfig({
   api: {
     projectId: '1oobv90f',
--- my-app/src/client.ts
+++ my-app/src/client.ts
@@ -1,15 +1,12 @@
 import {createClient} from '@sanity/client';
-import { groqStore } from '@sanity/groq-store';
 
 const sanityClient = createClient({
-  projectId: '1oobv90f',
+  projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
   dataset: 'production',
   useCdn: false,
   apiVersion: "2024-06-11",
   //token: to be inserted,
   token: process.env.REACT_APP_SANITY_TOKEN,
 });
-
-
 
 export default sanityClient;
