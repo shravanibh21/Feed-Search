546dbb0380968aa67bf044d3aba7233609759181
Shravani Bhujbal <shravanibh21@gmail.com>
2024-07-08 18:51:54 +0530

    Update client.ts

--- my-app/src/client.ts
+++ my-app/src/client.ts
@@ -5,7 +5,7 @@ const sanityClient = createClient({
   dataset: 'production',
   useCdn: false,
   apiVersion: "2024-06-11",
-  token: process.env.REACT_APP_SANITY_TOKEN,
+  token: 'skB3fudBGpoEODP8S6ix12OI4ox2iEi7sLZfRNvdot6Cw1QpFe6iOZseAI2NmdMJ4xY6OQw36TlT5zXqlxGt8DyXeDIyhliS55yJ6jiXFQyOiwa1PES1t46JHusA5bRJGaam9SD2sgj71bLJZdEz1r4htHEBQrX08E5yUySzQDy75nMWQfy2',
 });
 
 export default sanityClient;
